1802031274add79fa4eb4729c04f4c8855d09afc
yashk0thari <yashkothari1000@g.ucla.edu>
2022-11-01 11:40:37 -0700

    Initial Commit: Initialized Client + Server

---
+++ server/index.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+    res.send("Hello, World!");
+});
+
+app.listen(3001, () => {
+    console.log("Running on port 3001.");
+});
