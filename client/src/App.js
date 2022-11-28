1296a649480cf146495a0b3f24e4e850b0442ec5
arjunloomba1311 <arjunrajloomba@gmail.com>
2022-11-27 23:57:00 -0800

    minor changes to connect React and Node with Proxy - still untested

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,28 @@
 import './App.css';
-import React from 'react';
+import React, { Component } from 'react';
+
+class App extends Component {
+
+  state = {tasks: []}
+  componentDidMount() {
+    fetch('/getAll/')
+    .then((res = res.json())
+    .then((users => this.setState({tasks})))
+    )
+  }
+  
+  render() {
+    return {
+    }
+  }
+
+
+}
 
 function App() {
   return (
     <div className="App">
+
     </div>
   );
 }
